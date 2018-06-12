3688d3b503c06de91e84b3582f23c0399a0e7649
webtaken <luckly083@gmail.com>
2018-06-11 21:13:56 -0500

    Update BGCD.cpp

--- investigacion/MCD/BGCD.cpp
+++ investigacion/MCD/BGCD.cpp
@@ -3,29 +3,20 @@
 #include <math.h>
 
 using namespace std;
-
-int BGCD(int a, int b){//es mucho mas rapido que el MCD clasico
-  int pot = 0;
-  cout << "a" << " b " << endl;
-  cout << a << " " << b << endl; 
-  while ( a != 0 && b != 0 && a != b ) {
-    while ( !(a & 1) && !(b & 1) ) {//si a y b son pares => MCD(a , b) = 2 * MCD(a/2 , b/2)
-      pot++;
-      a >>= 1;//a = a / 2^1
-      b >>= 1;//b = b / 2^1
+int BGCD(int a , int b){
+  int e = 1;
+    while ( !(a & 1) && !(b & 1) ) {
+      e <<= 1;
+      a >>= 1;
+      b >>= 1;
     }
-    if ( !(a & 1) && (b & 1) ) a >>= 1; //si 2 solo divide a 'a' => MCD(a , b) = MCD(a/2 , b)
-    else if ( (a & 1) && !(b & 1) ) b >>= 1;//si 2 solo divide a 'b' => MCD(a , b) = MCD(a , b/2)
-    else if(a >= b) a = (a - b) >> 1;
-    else{
-      int tmp = (b - a) >> 1;
-      a = b;
-      b = tmp;
-    }
+  while ( a ^ 0 ) {
+    while ( !(a & 1) ) a >>= 1;
+    while ( !(b & 1) ) b >>= 1;
+    if(a >= b) a=a-b; else b=b-a;
   }
-  return b * pow(2 , pot);// return b*2^pot
+  return b * e;
 }
-
 
 int main(){
   int a,b;
